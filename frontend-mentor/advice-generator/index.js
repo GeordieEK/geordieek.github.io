b1897313359507559664c7965df50a995b288452
GeordieEK <geordieek@gmail.com>
2023-06-23 14:21:36 +1000

    Added html landmarks and better function encapsulation in index.js

--- frontend-mentor/advice-generator/index.js
+++ frontend-mentor/advice-generator/index.js
@@ -1,24 +1,31 @@
-const elements = {
-    adviceButton : document.querySelector("#diceButton"),
-    adviceNum : document.querySelector("#adviceNum"),
-    advice : document.querySelector("#advice")
+const getElements = () => {
+    return {
+        adviceButton : document.querySelector("#diceButton"),
+        adviceNum : document.querySelector("#adviceNum"),
+        advice : document.querySelector("#advice")
+    }
 }
 
 const getAdvice = async () => {
     try {
         const adviceRequest = await fetch("https://api.adviceslip.com/advice");
         const advice = await adviceRequest.json();
-        console.log(advice);
-        elements.advice.innerText = advice.slip.advice;
-        elements.adviceNum.innerText = `Advice #${advice.slip.id}`;
+        return advice;
     }
     catch (e) {
         console.error(e);
     }
 }
 
+const displayAdvice = () => {
+    advice = getAdvice();
+    const elements = getElements();
+    elements.advice.innerText = advice.slip.advice;
+    elements.adviceNum.innerText = `Advice #${advice.slip.id}`;
+}
+
 const addListeners = () => {
-    elements.adviceButton.addEventListener("click", getAdvice)
+    elements.adviceButton.addEventListener("click", displayAdvice)
 }
 
 // add event listeners
