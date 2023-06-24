cafe4d1ad10bfdaede4c259ca2dec6c14a29ec26
GeordieEK <geordieek@gmail.com>
2023-06-24 10:43:22 +1000

    Improved formatting

--- frontend-mentor/advice-generator/index.js
+++ frontend-mentor/advice-generator/index.js
@@ -1,32 +1,31 @@
 const getElements = () => {
-    return {
-        adviceButton : document.querySelector("#diceButton"),
-        adviceNum : document.querySelector("#adviceNum"),
-        advice : document.querySelector("#advice")
-    }
-}
+  return {
+    adviceButton: document.querySelector('#diceButton'),
+    adviceNum: document.querySelector('#adviceNum'),
+    advice: document.querySelector('#advice'),
+  };
+};
 
 const getAdvice = async () => {
-    try {
-        const adviceRequest = await fetch("https://api.adviceslip.com/advice");
-        const advice = await adviceRequest.json();
-        return advice;
-    }
-    catch (e) {
-        console.error(e);
-    }
-}
+  try {
+    const adviceRequest = await fetch('https://api.adviceslip.com/advice');
+    const advice = await adviceRequest.json();
+    return advice;
+  } catch (e) {
+    console.error(e);
+  }
+};
 
 const displayAdvice = () => {
-    advice = getAdvice();
-    const elements = getElements();
-    elements.advice.innerText = advice.slip.advice;
-    elements.adviceNum.innerText = `Advice #${advice.slip.id}`;
-}
+  advice = getAdvice();
+  const elements = getElements();
+  elements.advice.innerText = advice.slip.advice;
+  elements.adviceNum.innerText = `Advice #${advice.slip.id}`;
+};
 
 const addListeners = () => {
-    elements.adviceButton.addEventListener("click", displayAdvice)
-}
+  elements.adviceButton.addEventListener('click', displayAdvice);
+};
 
 // add event listeners
 addListeners();
